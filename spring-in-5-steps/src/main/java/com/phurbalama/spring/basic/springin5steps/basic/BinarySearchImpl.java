add9b05ac5ba23c1b017862f7534fa65bce8817b
Phurba Lama <phurbalama2002@gmail.com>
2021-11-27 23:44:44 -0500

    [Spring Application - 11/27/2021]

--- spring-in-5-steps/src/main/java/com/phurbalama/spring/basic/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/phurbalama/spring/basic/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.phurbalama.spring.basic.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -8,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class BinarySearchImpl {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	//this is a dependency it means sortAlgorithm is a dependency for BinarySearch
 	@Autowired
 	@Qualifier("quick")
@@ -28,6 +34,18 @@ public class BinarySearchImpl {
 		
 		//Return the result
 		return 3;
+	}
+	
+	//as soon as bean is created, post construct is called
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postconstruct");
+	}
+	
+	//when bean instance is being removed pre destroy is called 
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("preDestroy");
 	}
 	
 	
